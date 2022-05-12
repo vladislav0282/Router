4d4d574ccabb055dbded8019dedabab9088b338c
vladislav0282 <ubpsv31460@yandex.ru>
2022-05-13 00:51:42 +0300

    Add Router

---
+++ app_rout_spa/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import Header from './Header';
+import About from './About';
+import Users from './Users';
+import Main from './Main';
+
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      {/* <Header/> */}
+      <Router>
+        <nav>
+          <li><Link to="/">Main</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/users">Users</Link></li>
+        </nav>
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route path="/about" component={About} />
+          <Route path="/users" component={Users} />
+        </Switch>
+      </Router>
+    </>
+  );
+}
+
+export default App;
